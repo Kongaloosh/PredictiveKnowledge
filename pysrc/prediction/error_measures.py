Fixed the broadcasting issues that arise with multiple layers. It would benefit from a more critical look, but I believe it's good for now.

pysrc/prediction/error_measures.py
@@ -19,8 +19,8 @@ def update_rupee(beta_naught, tau, delta_e, h, e, delta, alpha, phi):
          """
     tau = (1 - beta_naught) * tau + beta_naught
     beta = beta_naught / tau
-    delta_e = (1 - beta) * delta_e + beta * e * delta
-    h += alpha * (delta * e - (np.inner(h, phi) * phi))
+    delta_e = (1 - beta) * delta_e + beta * e * delta.T
+    h += alpha * (delta.T * e - (np.inner(h, phi)[:, None] * phi))
     return np.sqrt(np.abs(np.inner(h, e))), tau, delta_e, h
 
 
@@ -36,11 +36,11 @@ def update_ude(beta, delta_average, delta_variance, delta):
         (ude, delta_average, delta_variance)
         """
     if delta_average is None:
-        delta_average = delta
+        delta_average = delta.T
         delta_variance = np.zeros(delta.shape)
     else:
-        err = delta - delta_average
-        delta_average += err * beta
+        err = delta.T - delta_average
+        delta_average = delta_average + (err * beta).T
         delta_variance = (1-beta) * (delta_variance + beta * err**2)
     return np.abs(delta_average / (np.sqrt(delta_variance) + 0.001))[:, 0], delta_average, delta_variance
 

pysrc/prediction/network/off_policy_horde.py
@@ -59,7 +59,7 @@ class HordeLayer(object):
         self.step_sizes = np.ones((num_predictions, self.function_approximation.dimensions)) * step_sizes
         self.bias_correction = np.zeros(self.weights.shape)
         self.eligibility_traces = np.zeros(self.weights.shape)
-        self.traces_lambda = traces_lambda[:, None]
+        self.traces_lambda = traces_lambda
         self.policies = policies
         self.step_size_bias_correction = 0.0001 # from AW's thesis; no idea how to set it well
 
@@ -238,7 +238,7 @@ class HordeLayer(object):
             # update the traces based on the new visitation
             self.eligibility_traces = accumulate(self.eligibility_traces, discounts, self.traces_lambda, phi_next, rho)
             # calculate the new cumulants
-            current_cumulants = np.array([cumulant.cumulant(observations) for cumulant in self.cumulants])[:, None]
+            current_cumulants = np.array([cumulant.cumulant(observations) for cumulant in self.cumulants])
             # get a vector of TD errors corresponding to the performance.
             td_error = calculate_temporal_difference_error(self.weights, current_cumulants, discounts, phi_next,
                                                            self.last_phi)

pysrc/prediction/off_policy/gtd.py
@@ -52,7 +52,8 @@ def calculate_temporal_difference_error(weights, cumulant, gamma_next, phi_next,
     Returns:
         td_error: the temporal-difference error for the current observations.
     """
-    return cumulant + gamma_next * np.inner(weights, phi_next) - np.inner(weights, phi_next)
+
+    return cumulant + gamma_next * np.dot(weights, phi_next) - np.dot(weights, phi_next)
 
 
 def update_weights(td_error, traces, weights, gamma, lmbda, step_size, phi_next, h):
@@ -69,7 +70,8 @@ def update_weights(td_error, traces, weights, gamma, lmbda, step_size, phi_next,
     Returns:
         weights: updated weights correcting for current TD error.
     """
-    return weights + step_size * (td_error * traces - gamma * (1 - lmbda) * np.inner(traces, h) * phi_next.T)
+
+    return weights + step_size * (td_error.T * traces - gamma * (1 - lmbda) * np.sum(traces * h, axis=1)[:, None] * phi_next)
 
 
 def update_h_trace(h, td_error, step_size,traces,phi):
@@ -83,4 +85,4 @@ def update_h_trace(h, td_error, step_size,traces,phi):
     Returns:
         h: updated bias correction term.
     """
-    return h + step_size * (td_error * traces - np.inner(h, phi) * phi.T)
+    return h + step_size * (td_error.T * traces - np.dot(h, phi)[:, None] * phi.T)